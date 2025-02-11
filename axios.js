23c2a4cc2de7ac31a3180b031f31b2305be8bce8
Amazing-Tech1 <kelvinmakinde2@gmail.com>
2025-02-11 04:40:14 +0100

    new commit

--- axios.js
+++ axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
 export default axios.create({
-    baseURL: 'http://localhost:5000',
+    baseURL: 'https://delivey-app-backend.vercel.app',
     withCredentials: true,
 })
